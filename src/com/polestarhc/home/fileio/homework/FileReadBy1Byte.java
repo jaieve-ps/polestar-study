54e0cb5daede2e16c90e00d1a48193f8d808fce2
jaieve <yoori0303@naver.com>
2021-08-19 01:02:30 +0900

    Bytes check for different types of files

---
+++ src/com/polestarhc/home/fileio/homework/FileReadBy1Byte.java
@@ -0,0 +1,32 @@
+package com.polestarhc.home.fileio.homework;
+
+import java.io.ByteArrayOutputStream;
+import java.io.FileInputStream;
+
+public class FileReadBy1Byte {
+    public static void main(String[] args) throws Exception {
+        FileReadBy1Byte fileReadBy1Byte = new FileReadBy1Byte();
+//        fileReadBy1Byte.readByte("fr.txt");
+//        fileReadBy1Byte.readByte("image.jpg");
+//        fileReadBy1Byte.readByte("PACS별연동구분정보.xlsx");
+        fileReadBy1Byte.readByte("협조기안방법_최종.pdf");
+    }
+
+    private void readByte(String path) throws Exception {
+        FileInputStream fis = new FileInputStream("testfileForFileIO/"+path);
+        ByteArrayOutputStream baos = new ByteArrayOutputStream();
+        int bytesize = 0;
+        String string = null;
+        byte[] aByte = new byte[1];
+        for (int i = 0; i < 100; i++){
+            bytesize=fis.read(aByte);
+            baos.write(aByte);
+            byte[] byteArraytoPrint = baos.toByteArray();
+            System.out.print(i+" 번째 Byte Format: "+ byteArraytoPrint.toString() + i +" 번째 Ascii Format: ");
+            System.out.print((int)byteArraytoPrint[0]);
+            System.out.println(", Char Format: "+(char)byteArraytoPrint[0]);
+            baos.reset();
+            aByte = new byte[1];
+        }
+    }
+}
